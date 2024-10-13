23639b2cd21633151a6238cfc9ea4e77815ab483
ovi100 <ovi0167@hotmail.com>
2024-10-14 00:43:40 +0600

    [FIXED] project details page icon undefine issue fixed

--- app/project/[id]/page.js
+++ app/project/[id]/page.js
@@ -2,28 +2,6 @@ import { person } from "@/app/data";
 import Image from "next/image";
 
 const AppDetails = ({ params }) => {
-  if (!params.id) {
-    return (
-      <div className="text-center p-5">
-        <svg
-          aria-hidden="true"
-          class="inline w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-blue-500"
-          viewBox="0 0 100 101"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-            fill="currentColor"
-          />
-          <path
-            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-            fill="currentFill"
-          />
-        </svg>
-      </div>
-    );
-  }
 
   const appItem = person.projects.find((app) => app.id === Number(params?.id));
 
@@ -35,18 +13,18 @@ const AppDetails = ({ params }) => {
             <div className="logo w-1/4 sm:w-auto">
               <Image
                 className="w-16 h-16 sm:w-20 sm:h-20 rounded-full"
-                src={appItem.icon}
-                alt={appItem.name}
+                src={appItem?.icon}
+                alt={appItem?.name}
               />
             </div>
             <div className="text w-3/4 sm:w-auto">
               <div className="text text-xl sm:text-2xl line-clamp-2 font-semibold capitalize">
-                {appItem.name}
+                {appItem?.name}
               </div>
               <div className="flex items-center gap-x-1 text-xs sm:text-sm mt-2.5">
                 <div className="text flex items-center gap-x-1 text-xs border border-black rounded-3xl text-black capitalize px-3 py-1">
                   <span className="hidden sm:block">
-                    {appItem.platform === "android" ? (
+                    {appItem?.platform === "android" ? (
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
@@ -78,7 +56,7 @@ const AppDetails = ({ params }) => {
                       </svg>
                     )}
                   </span>
-                  {appItem.platform}
+                  {appItem?.platform}
                 </div>
               </div>
             </div>
@@ -86,15 +64,15 @@ const AppDetails = ({ params }) => {
           <div className="button w-1/4 sm:w-auto">
             <a
               className={`block px-3 py-4 leading-[0] ${
-                appItem.platform === "android"
+                appItem?.platform === "android"
                   ? "bg-green-500 hover:bg-green-600"
                   : "bg-orange-500 hover:bg-orange-600"
               } text-sm font-medium text-center text-white rounded focus:ring-0 focus:outline-none capitalize`}
-              href={appItem.url}
+              href={appItem?.url}
               target="_blank"
               rel="noopener noreferrer"
             >
-              {appItem.platform === "android" ? (
+              {appItem?.platform === "android" ? (
                 <span className="flex items-center gap-x-1.5">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -140,8 +118,8 @@ const AppDetails = ({ params }) => {
               description
             </div>
             <div className="text-sm text-slate-400 mt-3">
-              {appItem.description?.charAt(0).toUpperCase() +
-                appItem.description?.slice(1).toLowerCase()}
+              {appItem?.description?.charAt(0).toUpperCase() +
+                appItem?.description?.slice(1).toLowerCase()}
             </div>
           </div>
           <h1 className="mt-10 text-5xl">This page is under construction</h1>
